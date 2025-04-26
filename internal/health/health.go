fe43bb92e76cfa5d7eead425a1514a03f9a8d528
Muhammad Daffa Dinaya <muhammaddaffadinaya@gmail.com>
2025-04-26 17:31:17 +0700

    docs: remove doc annotation

--- internal/health/health.go
+++ internal/health/health.go
@@ -6,15 +6,6 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// HealthCheck godoc
-//
-//	@Summary		Show the status of server.
-//	@Description	get the status of server.
-//	@Tags			root
-//	@Accept			*/*
-//	@Produce		json
-//	@Success		200	{object}	map[string]interface{}
-//	@Router			/health [get]
 func HealthCheck(context echo.Context) error {
 	return context.JSON(http.StatusOK, map[string]interface{}{
 		"data": "Server is up and running",
